13d8f45754cc0b6723ff1527f449668b8276caee
Kienroro2003 <91589673+Kienroro2003@users.noreply.github.com>
2023-11-17 17:12:11 +0700

    fixed folder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Header from "./pages/Header";
 import "./sass/index.scss";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Kienroro</h1>
+      <Header></Header>
     </div>
   );
 }
---
+++ src/pages/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import "./Header.scss";
+
+const Header = () => {
+  return (
+    <div>
+      <h3 className="title">Header</h3>
+    </div>
+  );
+};
+
+export default Header;
