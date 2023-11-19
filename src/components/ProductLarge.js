ecd53f858b60243163925c481106884e5c453da3
Kienroro2003 <91589673+Kienroro2003@users.noreply.github.com>
2023-11-19 19:58:08 +0700

    update product produc

--- src/components/ProductLarge.js
+++ src/components/ProductLarge.js
@@ -1,7 +1,32 @@
 import React from "react";
+import Product from "../assets/images/categories/product-large.png";
+import { ReactComponent as Heart } from "../assets/icons/heart.svg";
+import { ReactComponent as Star } from "../assets/icons/star.svg";
 
 const ProductLarge = () => {
-  return <div></div>;
+  return (
+    <div className="product-large">
+      <div className="product-large__block">
+        <img src={Product} alt="Product Large" className="product-large__img" />
+        <div className="product-large__icon">
+          <Heart className="icon"></Heart>
+        </div>
+      </div>
+      <div className="product-large__content">
+        <h3 className="product-large__heading">
+          Coffee Beans - Espresso Arabica and Robusta Beans
+        </h3>
+        <div className="product-large__brand">Lavazza</div>
+        <div className="flex justify-between">
+          <div className="product-large__price">$47.00</div>
+          <div className="product-large__rating">
+            <Star></Star>
+            <div className="product-large__core">5.0</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductLarge;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Hero from "../components/Hero";
 import ProductMini from "../components/ProductMini";
+import ProductLarge from "../components/ProductLarge";
+import Filter from "../components/Filter";
 
 const Home = () => {
   return (
@@ -8,8 +10,8 @@ const Home = () => {
       <div className="container">
         <div className="home__container">
           <Hero></Hero>
-          <section className="home-category">
-            <h2 className="home-category__heading">Browse Categories</h2>
+          <section className="home-section">
+            <h2 className="home-section__heading">Browse Categories</h2>
             <ul className="home-category__list">
               <li className="home-category__item">
                 <ProductMini></ProductMini>
@@ -19,6 +21,26 @@ const Home = () => {
               </li>
               <li className="home-category__item">
                 <ProductMini></ProductMini>
+              </li>
+            </ul>
+          </section>
+          <section className="home-section">
+            <div className="flex items-center justify-between">
+              <h2 className="home-section__heading">Browse Categories</h2>
+              <Filter></Filter>
+            </div>
+            <ul className="product-large__list">
+              <li className="product-large__item">
+                <ProductLarge></ProductLarge>
+              </li>
+              <li className="product-large__item">
+                <ProductLarge></ProductLarge>
+              </li>
+              <li className="product-large__item">
+                <ProductLarge></ProductLarge>
+              </li>
+              <li className="product-large__item">
+                <ProductLarge></ProductLarge>
               </li>
             </ul>
           </section>
