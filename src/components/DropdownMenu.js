40eecb9884e7aba530d926c63cb93703686440e5
Kienroro2003 <91589673+Kienroro2003@users.noreply.github.com>
2023-11-21 23:36:23 +0700

    update dropdown data

--- src/components/DropdownMenu.js
+++ src/components/DropdownMenu.js
@@ -1,15 +1,4 @@
-import Square from "../assets/icons/shapes/square.svg";
-// import Rectange from "../assets/icons/shapes/rectange.svg";
-// import Moon from "../assets/icons/shapes/moon.svg";
-// import Hexagon from "../assets/icons/shapes/hexagon.svg";
-// import Camputer from "../assets/icons/categories/compute.svg";
-// import Phone from "../assets/icons/categories/phone.svg";
-// import Shield from "../assets/icons/categories/shield.svg";
-// import Star from "../assets/icons/shapes/star.svg";
-// import CupYoutube from "../assets/icons/categories/cup-youtube.svg";
 import React, { Fragment, useEffect, useRef } from "react";
-import Circle from "../assets/icons/shapes/circle.svg";
-import FourSquare from "../assets/icons/categories/four-square.svg";
 import DropdownSubmenu from "./DropdownSubmenu";
 
 const DropdownMenu = (props) => {
@@ -19,12 +8,6 @@ const DropdownMenu = (props) => {
         <ul className="dropdown-menu">
           {props.data &&
             props.data.map((item) => {
-              // let mainIcon = require(item["main-icon"]);
-              // console.log(
-              //   "🚀 ~ file: DropdownMenu.js:23 ~ DropdownMenu ~ mainIcon:",
-              //   mainIcon
-              // );
-              // let subIcon = require(item["sub-icon"]);
               return (
                 <Fragment>
                   <li
@@ -33,12 +16,14 @@ const DropdownMenu = (props) => {
                     }`}
                   >
                     <div className="dropdown-menu__item-icons">
-                      <img src={item["main-icon"]} alt="" />
-                      <img
-                        src={item["sub-icon"]}
-                        alt=""
-                        className="dropdown-menu__item-icon icon"
-                      />
+                      <div className="dropdown-menu__item-block">
+                        <img src={item["main-icon"]} alt="" />
+                        <img
+                          src={item["sub-icon"]}
+                          alt=""
+                          className="dropdown-menu__item-icon icon"
+                        />
+                      </div>
                     </div>
                     <div className="dropdown-menu__body">
                       <a href="#!" className="dropdown-menu__link">
