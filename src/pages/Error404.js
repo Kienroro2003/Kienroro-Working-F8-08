1827afa48e104391bc50acda5bd221c23c38cb29
Kienroro2003 <91589673+Kienroro2003@users.noreply.github.com>
2023-11-23 21:46:53 +0700

    update handle 4040 age

---
+++ src/pages/Error404.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Error404 = () => {
+  return (
+    <>
+      <div className="error">
+        <img
+          src="./assets/images/error-404.jpg"
+          alt=""
+          className="error__image"
+        />
+      </div>
+    </>
+  );
+};
+
+export default Error404;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
-import { Home, SignIn, SignUp } from "../pages";
+import { Home, SignIn, SignUp, Error404 } from "../pages";
 import { AuthLayout, Layout } from "../layouts";
 
 const routers = [
   { path: "/", layout: Layout, component: Home },
   { path: "/sign-in", layout: AuthLayout, component: SignIn },
   { path: "/sign-up", layout: AuthLayout, component: SignUp },
+  { path: "*", layout: AuthLayout, component: Error404 },
 ];
 
 export { routers };
